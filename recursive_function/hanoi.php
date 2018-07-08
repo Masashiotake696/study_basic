8b7f1a6a374ebeaec9a4240ab111c0bf1c10c259
Masashiotake696 <masashi.otake696@gmail.com>
2018-07-08 21:09:20 +0900

    ハノイの塔リファクタリング

--- recursive_function/hanoi.php
+++ recursive_function/hanoi.php
@@ -1,122 +1,46 @@
 <?php
 
 // 解くハノイの塔を定義
-$hanoi = [ [ 1, 2, 3, 4, 5], [], [] ];
-// $hanoi = [ [ 1, 2, 3, 4, 5 ], [], [] ];
+$hanoi = [ [ 1, 2, 3, 4, 5 ], [], [] ];
 // 移動前のハノイの塔を出力
-// var_dump($hanoi);
-// var_dump($hanoi);
-$hanoi = moveMiddle($hanoi, 0, count($hanoi[0]));
+var_dump($hanoi);
+$hanoi = moveHanoi($hanoi, 0, 1, count($hanoi[0]));
 
 // 移動後のハノイの塔を出力
 var_dump($hanoi);
 
-function moveMiddle($tower, $position, $number) {
-    // 要素が一つの場合は中央に移動させる
+/**
+ * ハノイの塔の要素を移動させる
+ *
+ * @param array $tower ハノイの塔の要素配列
+ * @param string $now 移動前(現在)位置
+ * @param string $after 移動先位置
+ * @param integer $number 移動させる要素の個数
+ * @return array 移動後のハノイの塔要素配列
+ */
+function moveHanoi($tower, $now, $after, $number) {
+    // 要素が一つの場合は移動
     if($number === 1) {
-        $buf = $tower[$position][0];
-        array_splice($tower[$position], 0, 1);
-        array_unshift($tower[1], $buf);
+        // 一時変数に退避
+        $buf = $tower[$now][0];
+        // 現在の位置から要素を削除
+        array_splice($tower[$now], 0, 1);
+        // 移動後の位置に要素を追加
+        array_unshift($tower[$after], $buf);
         return $tower;
     }
 
-    // 一番下の要素以外を左右に移動させる
-    if($position === 0) { // 左
-        var_dump($tower, $position, $number, 'middle function: moveRight');
-        echo "-----------------------------\n";
-        $tower = moveRight($tower, $position, $number - 1);
-    } else { // 右
-        var_dump($tower, $position, $number, 'middle function: moveLeft');
-        echo "-----------------------------\n";
-        $tower = moveLeft($tower, $position, $number - 1);
-    }
-    // 一番下の要素を中央に移動
-    var_dump($tower, $position, $number, 'middle function:  moveMiddle');
-    echo "-----------------------------\n";
-    $tower = moveMiddle($tower, $position, 1);
-    // 左右に移動させた要素を中央に移動させる
-    if($position === 0) {
-        var_dump($tower, $position, $number, 'middle function: moveMiddle from right');
-        echo "-----------------------------\n";
-        $tower = moveMiddle($tower, 2, $number - 1);
-    } else {
-        var_dump($tower, $position, $number, 'middle function: moveMiddle from left');
-        echo "-----------------------------\n";
-        $tower = moveMiddle($tower, 0, $number - 1);
-    }
+    // 退避位置を取得
+    $evacuation_name = array_values(array_diff([0, 1, 2], [$now, $after]))[0];
 
-    return $tower;
-}
+    // 一番下の要素以外を退避位置に移動させる
+    $tower = moveHanoi($tower, $now, $evacuation_name, $number - 1);
 
-function moveLeft($tower, $position, $number) {
-    if($number === 1) {
-        $buf = $tower[$position][0];
-        array_splice($tower[$position], 0, 1);
-        array_unshift($tower[0], $buf);
-        return $tower;
-    }
+    // 一番下の要素を目的位置に移動
+    $tower = moveHanoi($tower, $now, $after, 1);
 
-    // 一番下の要素以外を中央か右に移動させる
-    if($position === 1) {
-        var_dump($tower, $position, $number, 'left function: moveRight');
-        echo "-----------------------------\n";
-        $tower = moveRight($tower, $position, $number - 1);
-    } else {
-        var_dump($tower, $position, $number, 'left function: moveMiddle');
-        echo "-----------------------------\n";
-        $tower = moveMiddle($tower, $position, $number - 1);
-    }
-    // 一番下の要素を左に移動させる
-    var_dump($tower, $position, $number, 'left function: moveLeft');
-    echo "-----------------------------\n";
-    $tower = moveLeft($tower, $position, 1);
-
-    // 中央か右に移動させた要素を左に移動させる
-    if($position === 1) {
-        var_dump($tower, $position, $number, 'left function: moveLeft from right');
-        echo "-----------------------------\n";
-        $tower = moveLeft($tower, 2, $number - 1);
-    } else {
-        var_dump($tower, $position, $number, 'left function: moveLeft from middle');
-        echo "-----------------------------\n";
-        $tower = moveLeft($tower, 1, $number - 1);
-    }
-
-    return $tower;
-}
-
-function moveRight($tower, $position, $number) {
-    if($number === 1) {
-        $buf = $tower[$position][0];
-        array_splice($tower[$position], 0, 1);
-        array_unshift($tower[2], $buf);
-        return $tower;
-    }
-
-    // 一番下の要素以外を中央か左に移動させる
-    if($position === 1) {
-        var_dump($tower, $position, $number, 'right function: moveLeft');
-        echo "-----------------------------\n";
-        $tower = moveLeft($tower, $position, $number - 1);
-    } else {
-        var_dump($tower, $position, $number, 'right function: moveMiddle');
-        echo "-----------------------------\n";
-        $tower = moveMiddle($tower, $position, $number - 1);
-    }
-    // 一番下の要素を右に移動させる
-    var_dump($tower, $position, $number, 'right function:  moveRight');
-    echo "-----------------------------\n";
-    $tower = moveRight($tower, $position, 1);
-    // 中央か左に移動させた要素を右に移動させる
-    if($position === 1) {
-        var_dump($tower, $position, $number, 'right function: moveRight from left');
-        echo "-----------------------------\n";
-        $tower = moveRight($tower, 0, $number - 1);
-    } else {
-        var_dump($tower, $position, $number, 'right function: moveRight from middle');
-        echo "-----------------------------\n";
-        $tower = moveRight($tower, 1, $number - 1);
-    }
+    // 退避位置に移動させた要素を目的位置に移動させる
+    $tower = moveHanoi($tower, $evacuation_name, $after, $number - 1);
 
     return $tower;
 }
