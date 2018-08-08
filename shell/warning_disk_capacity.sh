4466e96b03f6839ea6ee3ba54602f2f20f9359e1
Masashiotake696 <masashi.otake696@gmail.com>
2018-08-08 09:51:44 +0900

    warning_disk_capacityの微修正

--- shell/warning_disk_capacity.sh
+++ shell/warning_disk_capacity.sh
@@ -2,7 +2,7 @@
 
 capacity=`df | sed -e '1d' | grep disk1s1 | awk '{print $5;}' | sed -e 's/%//'`
 
-if [ $capacity -gt 40 ]; then
+if [ $capacity -gt 80 ]; then
   echo "Warning!!!"
 fi
 echo "Now disk capacity is" $capacity
